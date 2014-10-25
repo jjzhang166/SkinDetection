f8ea8a5e6af2c496f885f7976a6ee31b7fbc9461
itachihellder <helder.garjunior@outlook.com>
2014-10-25 16:47:05 -0200

    its working now!!

--- SkinDetection/Main.cpp
+++ SkinDetection/Main.cpp
@@ -109,7 +109,6 @@ Mat searchForMovement(Mat thresholdImage, Mat &cameraFeed,Mat output,bool *moveD
 	int xMax = 0,yMax = 0,xMin = 50000,yMin = 50000;
 	bool objectDetected = false,peopleDetect = false;
 	Rect rect;
-	//these two vectors needed for output of findContours
 	vector<Vec4i> hierarchy;
 	vector<vector<Point>> contours;
 	Mat temp;
@@ -117,40 +116,30 @@ Mat searchForMovement(Mat thresholdImage, Mat &cameraFeed,Mat output,bool *moveD
 	
 	findContours(temp,contours,hierarchy,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_TC89_KCOS );
 	
-	//if contours vector is not empty, we have found some objects
 	if(contours.size()>0) objectDetected=true;
 	else objectDetected = false;
 
-	//find contours of filtered image using openCV findContours function
-	//findContours(temp,contours,hierarchy,CV_RETR_CCOMP,CV_CHAIN_APPROX_SIMPLE );// retrieves all contours
-	// retrieves external contours
-	
-	//if contours vector is not empty, we have found some objects
 	if(contours.size()>0) objectDetected=true;
 	else objectDetected = false;
 
 	if(objectDetected){
-		//the largest contour is found at the end of the contours vector
-		//we will simply assume that the biggest contour is the object we are looking for.
-		//make a bounding rectangle around the largest contour then find its centroid
-		//this will be the object's final estimated position.
+
 		vector<Point> largerContour;
 		int area = 0;
-		//this code get the max and minimal points at the contours
 
-		for(int i = 0;i < contours.size();i++){	
+		for(int i = 0;i < (int)contours.size();i++){	
 			if(contourArea(contours[i]) > 1000){
-				for(int j = 0;j < contours[i].size();j++){	
+				for(int j = 0;j < (int)contours[i].size();j++){	
 					if(area < contourArea(contours[i])){
 						largerContour = contours[i];
-						area = contourArea(contours[i]);
+						area = (int)contourArea(contours[i]);
 						peopleDetect = true;
 					}
 				}
 			}
 		}
 
-		for(int i = 0;i < largerContour.size();i++){
+		for(int i = 0;i < (int)largerContour.size();i++){
 			
 			if(xMax < largerContour[i].x){
 				xMax = largerContour[i].x;
@@ -257,25 +246,59 @@ void initCapture(VideoCapture* capture){
 	capture->set(CV_CAP_PROP_SATURATION,93); //saturation 93
 }
 
+void getSkinMat(Mat frame,Mat *outImage){
+	skindetector mySkinDetector;
+	vector<vector<Point>> *contours = new vector<vector<Point>>;
+	vector<Vec4i> hierarchy;
+	RNG rng;
+
+	int erosion_size = 2;
+    Mat element = getStructuringElement(cv::MORPH_CROSS,
+                                        cv::Size(erosion_size +1 ,erosion_size + 1),
+                                        cv::Point(erosion_size, erosion_size) );
+    
+
+	Scalar color = Scalar( rng.uniform(0,255), rng.uniform(0,255), rng.uniform(0,255) );
+
+	*outImage = mySkinDetector.getSkin(frame);
+
+	cv::erode(*outImage, *outImage, element);
+	cv::dilate(*outImage, *outImage, element);
+	
+}
+
+vector<vector<Point>> getContours(Mat image,int* x){
+	vector<vector<Point>> contours;
+	vector<Vec4i>hierarchy;
+	double contourArea = 0;
+
+	findContours(image,contours,hierarchy,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_SIMPLE,Point(0,0)); //Get Skin contours
+
+		for( int i = 0; i< (int)contours.size(); i++) {
+			if(cv::contourArea(contours[i]) > 300){
+				if(cv::contourArea(contours[i]) > contourArea){
+					contourArea = cv::contourArea(contours[i]);
+					*x = i;
+				}
+				drawContours( image, contours, i,Scalar(127,127,127), 1.5, 1, hierarchy, CV_16SC1, Point() );
+			}
+		}
+		return contours;
+}
+
 int main(int argc, const char *argv[]) {
     VideoCapture capture;
-    Mat cameraFeed,skinMat,fore,movement,back,leftFrame,rightFrame;
-    skindetector mySkinDetector;
-    vector<vector<Point> > contours,bContours;
-	vector<Vec4i> hierarchy,bHierarchy;
-	cv:RNG rng;	
+    Mat cameraFeed,leftSkinMat,rightSkinMat,leftFrame,rightFrame,skinMat;
+    vector<vector<Point> > leftContours,rightContours;
+	vector<Point2f> leftDirection(5),rightDirection(5);
+	vector<Vec4i> leftHierarchy,rightHierarchy;
 	KalmanFilter KF(4, 2, 0);
-	bool flag = false,moveDetect = false;
-	int numberOfPeople = 0;
-	movement = clearImage();	
 	Rect leftR,rightR,window,partRect;
-
-
-
-	// background detection properties
-	cv::BackgroundSubtractorMOG2 bg;
-    bg.nmixtures = 3;
-    bg.bShadowDetection = false;
+	bool lFlag = false,rFlag = false;
+	int numberOfPeople = 0;
+	int lTemp = 0,rTemp = 0;
+	int lX = 0,lDx = 0,rX = 0,rDx = 0;
+	
 
 	
 	//Init Kalman filter configurations
@@ -287,79 +310,97 @@ int main(int argc, const char *argv[]) {
 	initRectSize(0,0,50,windowHeight,&leftR);//Used to detect people in the left of the window.
 	initRectSize(windowWidth - 50,0,50,windowHeight,&rightR);//Used to detect people in the left of the window.
 
-    //Create a structuring element
-    int erosion_size = 2;
-    Mat element = getStructuringElement(cv::MORPH_CROSS,
-                                        cv::Size(erosion_size +1 ,erosion_size + 1),
-                                        cv::Point(erosion_size, erosion_size) );
-    
-
-	Scalar color = Scalar( rng.uniform(0,255), rng.uniform(0,255), rng.uniform(0,255) );
-
-    //start an infinite loop where webcam feed is copied to cameraFeed matrix
-    //all of our operations will be performed within this loop
-	int alpha = 0,beta = 0;
 	while(1){
-		double contourArea = 0;
+		double leftContourArea = 0;
+		double rightContourArea = 0;
 
 		capture.read(cameraFeed);
-
-		//printf("Linahs: %d\nColunas: %d\n",cameraFeed.rows,cameraFeed.cols);
-		
-		back = imageConvert(cameraFeed);
-
-		bg.operator ()(back,fore);
-		cv::erode(fore,fore,element);
-		cv::dilate(fore,fore,element);	
-
-		//movement = searchForMovement(fore,cameraFeed,movement,&moveDetect);
-		
-		//if(!moveDetect)
-			//movement = clearImage();
+	
+		//meraFeed = imageConvert(cameraFeed);
 
 		leftFrame = getImagePart(cameraFeed,leftR);
-		
-		skinMat = mySkinDetector.getSkin(leftFrame);
+		rightFrame = getImagePart(cameraFeed,rightR);
 
-		cv::erode(skinMat, skinMat, element);
-		cv::dilate(skinMat, skinMat, element);
-		cv::findContours(skinMat,contours,hierarchy,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_SIMPLE,Point(0,0)); //Get Skin contours
-	
-	
-		
+		getSkinMat(leftFrame,&leftSkinMat);
+		getSkinMat(rightFrame,&rightSkinMat);
+
 	    int j = -1;
+		int k = -1;
 
-		for( int i = 0; i< contours.size(); i++) {
-			if(cv::contourArea(contours[i]) > 700){
-				if(cv::contourArea(contours[i]) > contourArea){
-					contourArea = cv::contourArea(contours[i]);
-					j = i;
-				}
-				drawContours( skinMat, contours, i,color, 1.5, 1, hierarchy, CV_16SC1, Point() );
+		leftContours = getContours(leftSkinMat,&j);
+		rightContours = getContours(rightSkinMat,&k);
+
+		Moments lmu,rmu;	
+		Point2f lmc = -1,rmc = -1;
+		lDx = 0;
+		rDx = 0;
+
+
+		//printf("%d",j);
+
+		if(j < 0)
+			lTemp = 0;
+		if(k < 0)
+			rTemp = 0;
+
+
+		if(j >= 0 && lTemp < 5){
+			lmu = moments( leftContours[j], false );
+			lmc = Point2f( lmu.m10/lmu.m00 , lmu.m01/lmu.m00 ); 
+			leftDirection[lTemp] = Point2f( lmu.m10/lmu.m00 , lmu.m01/lmu.m00 );
+			//drawCross(cameraFeed, mc, Scalar(255, 0, 0), 5);
+			lTemp++;
+		}
+
+		if(k >= 0 && rTemp < 5){
+			rmu = moments(rightContours[k],false);
+			rmc = Point2f( rmu.m10/rmu.m00 , rmu.m01/rmu.m00 ); 
+			rightDirection[rTemp] = Point2f( rmu.m10/rmu.m00 , rmu.m01/rmu.m00 );
+			//drawCross(cameraFeed, mc, Scalar(255, 0, 0), 5);
+			rTemp++;
+		}
+ 
+
+		if(lTemp == 5){
+			lX = (int)leftDirection[0].x;
+			for(int i = 1;i < (int)leftDirection.size();i++){
+				lDx += (int)(lX - leftDirection[i].x); 
+			}
+		}
+		if(rTemp == 5){
+			rX = rightDirection[0].x;
+			for(int i = 0;i < (int)rightDirection.size();i++){
+				rDx += (int)(rX - rightDirection[i].x);
 			}
 		}
 
-		Moments mu;	
-		Point2f mc = -1;
-		
-		if(j >= 0){
-			mu = moments( contours[j], false );
-			//mc = Point2f( mu.nu20,mu.nu20 ); 
-			mc = Point2f( mu.m10/mu.m00 , mu.m01/mu.m00 ); 
-			drawCross(cameraFeed, mc, Scalar(255, 0, 0), 5);
+		//printf("ldx: %d jrdx: %d\n",lDx,rDx);
+
+		if(lmc.inside(window) && lFlag == false){
+			if(lDx < 0){
+				numberOfPeople++;
+				lFlag = true;
+				printf("%d ",numberOfPeople);
+			}
 		}
- 
-		if(mc.inside(window) && flag == false){
-			numberOfPeople++;
-			flag = true;
-			printf("%d ",numberOfPeople);
+		if(rmc.inside(window) && rFlag == false){
+			if(rDx > 0){
+				numberOfPeople++;
+				rFlag = true;
+				printf("%d ",numberOfPeople);
+			}
 		}
 
-		if(!mc.inside(window))
-			flag = false;
+		if(!lmc.inside(window))
+			lFlag = false;
+		if(!rmc.inside(window))
+			rFlag = false;
 	    
-		imshow("skinDetection",skinMat);
-		imshow("Original Image",cameraFeed);
+		getSkinMat(cameraFeed,&skinMat);
+
+		imshow("right",rightSkinMat);
+		imshow("left",leftSkinMat);
+		imshow("Original Image",skinMat);
         waitKey(30);
     }
     return 0;
